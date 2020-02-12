e6937d90de20edd15ecf0b865499f22d43319d4a
saeko9741 <saeco9741@gmail.com>
2020-02-12 12:34:21 +0000

    following follower

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,21 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   attachment :profile_image, destroy: false
 
+  #フォローする側のUserから見て、フォローされる側のUserを(中間テーブルを介して)集める。なので親はfollowing_id(フォローする側)
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  # 中間テーブルを介して「follower」モデルのUser(フォローされた側)を集めることを「followings」と定義
+  has_many :followings, through: :active_relationships, source: :follower
+
+  #フォローされる側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。なので親はfollower_id(フォローされる側)
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  # 中間テーブルを介して「following」モデルのUser(フォローする側)を集めることを「followers」と定義
+  has_many :followers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+    # 今自分(引数のuser)がフォローしようとしているユーザーがフォローされているユーザー(つまりpassive)の中から、引数に渡されたユーザー(自分)がいるかどうかを調べる
+    passive_relationships.find_by(following_id: user.id).present?
+  end
+
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, presence: true, length: {maximum: 20, minimum: 2}
   validates :introduction, length: { maximum: 50 }
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update]do
+  resource :relationships, only: [:create, :destroy]
+     get :follows, on: :member
+     get :followers, on: :member
+   end
   resources :books,only: [:index, :show, :edit, :update, :destroy]do
   resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
